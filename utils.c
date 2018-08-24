ec92cfd31e7909954c1ba4eaf2aa102609e4a951
Joe <joeblackmore97@gmail.com>
2018-08-24 22:33:45 +0100

    Add converting and printing utility functions

---
+++ utils.c
@@ -0,0 +1,41 @@
+#include "utils.h"
+
+int *integer_to_binary(unsigned long long int integer, int num_of_bits) {
+    int *binary = malloc(sizeof(int) * num_of_bits);
+    int i;
+    for(i = 0; i < num_of_bits; i++) {
+        binary[i] = 0;
+    }
+
+    for(i = 0; i < num_of_bits; i++) {
+        binary[i] = (int) (integer >> (num_of_bits-1-i));
+        if(binary[i]) integer = integer ^ (1 << (num_of_bits-1-i));
+    }
+
+    return binary;
+}
+
+int *string_to_binary(char *string_to_convert) {
+    int string_len = strlen(string_to_convert);
+
+    int i, j; 
+    int *binary = malloc(sizeof(int) * string_len * 8);
+    for(i = 0; i < string_len; i++) {
+        int ascii_val = string_to_convert[i];
+
+        for(j = 0; j < 8; j++) {
+            binary[i*8 + j] = ascii_val >> (7-j);
+            if(binary[i*8 + j]) ascii_val = ascii_val ^ (1 << (7-j));
+        }
+    }
+
+    return binary;
+}
+
+void print_binary_array(int *binary_array, int array_len) {
+    int i;
+    for(i = 0; i < array_len; i++){
+        printf("%d", binary_array[i]);
+    }
+    printf("\n");
+}
---
+++ utils.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+
+extern int *integer_to_binary(unsigned long long int integer, int num_of_bits);
+extern int *string_to_binary(char *string_to_convert);
+void print_binary_array(int *binary_array, int array_len);
