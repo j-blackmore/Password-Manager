5c877e41e461b654357a06452e5639726239b8bb
Joe <joeblackmore97@gmail.com>
2018-09-07 23:04:29 +0100

    Hide password input in the terminal

--- login.c
+++ login.c
@@ -1,8 +1,10 @@
 #include "login.h"
 #include "crypto.h"
+#include <termios.h>
 
 int create_profile();
 static int check_hash_matches(char *input, char *hash);
+static int read_password(char **input, size_t *size);
 
 const char *file_name = ".userprofile";
 
@@ -12,28 +14,29 @@ int login() {
         return 1;
     }
 
-    // profile exists so get name
+    // profile exists so get name and hash
     FILE *file_reader = fopen(file_name, "r");
     char *profile_name = read_line(file_reader);
-    printf("%s's profile found\n", profile_name);
+    char *hash = read_line(file_reader);
+    fclose(file_reader);
     
     // request password
+    printf("%s's profile found\n", profile_name);
     printf("Enter password:");
     char *input_password = NULL;
     size_t input_len;
-    getline(&input_password, &input_len, stdin);
-    // replace newline char with end of string
-    input_password[strlen(input_password)-1] = '\0';
+    if(read_password(&input_password, &input_len) != 0) {
+        // error
+        fprintf(stderr, "Error reading password\n");
+    }
     
     // compare hash with hashed password
-    char *hash = read_line(file_reader);
     if(check_hash_matches(input_password, hash)) {
         printf("Welcome!\n");
     } else {
         printf("Incorrect password\n");
     }
 
-    fclose(file_reader);
     return 1;
 }
 
@@ -43,4 +46,32 @@ int create_profile() {
 
 static int check_hash_matches(char *input, char *hash) {
     return strcmp(sha256(input), hash) == 0 ? 1 : 0;
+}
+
+static int read_password(char **input, size_t *size) {
+    // get terminal info
+    struct termios termInfo;
+    if(tcgetattr(STDERR_FILENO, &termInfo) != 0) {
+        return 1;
+    }
+
+    // turn off terminal echoing
+    termInfo.c_lflag &= ~ECHO;
+    if(tcsetattr(STDIN_FILENO, TCSANOW, &termInfo) != 0) {
+        return 1;
+    } 
+
+    // read password and replace newline with end of string char
+    getline(input, size, stdin);
+    *input[strlen(*input)-1] = '\0';
+
+    // turn on terminal echoing
+    termInfo.c_lflag |= ECHO;
+    if(tcsetattr(STDIN_FILENO, TCSANOW, &termInfo) != 0) {
+        return 1;
+    }
+
+    // newline because enter was not output
+    printf("\n");
+    return 0;
 }
